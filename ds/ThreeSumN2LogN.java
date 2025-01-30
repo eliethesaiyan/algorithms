b13ebc0b41c0be7c90f7565d54edd20539544f4f
Eliethesaiyan <eliemagambo@gmail.com>
2025-01-30 17:59:17 +0900

    fixed bi search algo

--- ds/ThreeSumN2LogN.java
+++ ds/ThreeSumN2LogN.java
@@ -1,4 +1,3 @@
-import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.Arrays;
 
@@ -6,7 +5,12 @@ import edu.princeton.cs.algs4.StdOut;;
 public class ThreeSumN2LogN {
     private int[] N;
     public ThreeSumN2LogN(int capacity){
-        N = StdRandom.permutation(capacity, capacity);
+        N = new int[capacity];
+        for(int i = -(capacity - 1) / 2 ; i < capacity / 2; i++){
+           StdOut.println(i);
+           N[i + (capacity / 2)] = i;
+        }
+             
         Arrays.sort(N);
         int i = 0;
         while(i < N.length){
@@ -22,39 +26,42 @@ public class ThreeSumN2LogN {
         int count = 0;
         for(int i=0; i < N.length; i++){
             for(int j=i+1; j< N.length; j++){
-                if (-(N[i]+N[j]) == N[bisearch(i)])
+                StdOut.print("i: " + i);
+                if (N[i]+N[j] == N[bisearch(i)])
                      count++;
                 } 
     } 
     return count; }
 
     public int bisearch(int item){
-    int lowIndex =  0;
-    int highIndex = N.length;
-    int searchIndex =  N.length /2;
+    StdOut.println("search item "+ item);
+    int highIndex = N.length - 1 ;
+    int lowIndex = 0;
 
-    while(lowIndex< highIndex){
-        if (N[searchIndex] == item)
-            return  searchIndex;
+    while(lowIndex<= highIndex){
+    int searchIndex = lowIndex + (highIndex - lowIndex) /2;
+        if (item < N[searchIndex]){
+            highIndex = searchIndex - 1; 
+        }
+        else if(item > N[searchIndex]){
+            lowIndex = searchIndex + 1;
+        }
+        else  return  searchIndex;
 
-        if (N[searchIndex] < item)
-            highIndex = searchIndex; 
-        if (N[searchIndex] > item)
-            lowIndex = searchIndex;
-
-        StdOut.println(lowIndex);
+        StdOut.print("current item in the array\n");
         StdOut.println(N[searchIndex]);
+        StdOut.print("low index\n");
+        StdOut.println(lowIndex);
 
+        StdOut.print("high index\n");
         StdOut.println(highIndex);
 
     }
     return -1;
-
-
-    }
+}
 
  public static void main(String[] args){
-    ThreeSumN2LogN  obj = new ThreeSumN2LogN(20);
-    StdOut.println("row " + obj.threesum());
+    ThreeSumN2LogN  obj = new ThreeSumN2LogN(25);
+    StdOut.println("count " + obj.threesum());
  }
 }
